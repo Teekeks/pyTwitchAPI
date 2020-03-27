af557131f1a5a490e4ae43455ea36a73d01a8488
Teekeks <info@teawork.de>
2020-03-27 11:09:13 +0100

    added fields_to_enum helper

--- twitchAPI/helper.py
+++ twitchAPI/helper.py
@@ -1,10 +1,11 @@
 #  Copyright (c) 2020. Lena "Teekeks" During <info@teawork.de>
 import urllib.parse
 import uuid
-from typing import Union
+from typing import Union, List, Type, Optional
 from json import JSONDecodeError
 from aiohttp.web import Request
 from dateutil import parser as du_parser
+from enum import Enum
 
 
 TWITCH_API_BASE_URL = "https://api.twitch.tv/helix/"
@@ -48,24 +49,24 @@ async def get_json(request: 'Request') -> Union[list, dict, None]:
         return None
 
 
-def make_dict_field_datetime(data: dict, fields: list) -> dict:
-    fd = data
-    for key, value in data.items():
-        if isinstance(value, str):
-            if key in fields:
-                if value == "":
-                    fd[key] = None
-                else:
-                    fd[key] = du_parser.isoparse(value)
-        elif isinstance(value, dict):
-            fd[key] = make_dict_field_datetime(value, fields)
-        elif isinstance(value, list):
-            fd[key] = make_fields_datetime(value, fields)
-    return fd
-
-
-def make_fields_datetime(data: Union[dict, list], fields: list):
+def make_fields_datetime(data: Union[dict, list], fields: List[str]):
     """itterate over dict or list recursivly to replace string fields with datetime"""
+
+    def make_dict_field_datetime(data: dict, fields: list) -> dict:
+        fd = data
+        for key, value in data.items():
+            if isinstance(value, str):
+                if key in fields:
+                    if value == "":
+                        fd[key] = None
+                    else:
+                        fd[key] = du_parser.isoparse(value)
+            elif isinstance(value, dict):
+                fd[key] = make_dict_field_datetime(value, fields)
+            elif isinstance(value, list):
+                fd[key] = make_fields_datetime(value, fields)
+        return fd
+
     if isinstance(data, list):
         return [make_dict_field_datetime(d, fields) for d in data]
     else:
@@ -74,3 +75,33 @@ def make_fields_datetime(data: Union[dict, list], fields: list):
 
 def build_scope(scopes: list) -> str:
     return ' '.join([s.value for s in scopes])
+
+
+def fields_to_enum(data: Union[dict, list],
+                   fields: List[str],
+                   _enum: Type[Enum],
+                   default: Optional[Enum]) -> Union[dict, list]:
+    _enum_vals = [e.value for e in _enum.__members__.values()]
+    def make_dict_field_enum(data: dict,
+                             fields: List[str],
+                             _enum: Type[Enum],
+                             default: Optional[Enum]) -> dict:
+        fd = data
+        for key, value in data.items():
+            # TODO fix for non string values
+            if isinstance(value, str):
+                if key in fields:
+                    if value not in _enum_vals:
+                        fd[key] = default
+                    else:
+                        fd[key] = _enum(value)
+            elif isinstance(value, dict):
+                fd[key] = make_dict_field_enum(value, fields, _enum, default)
+            elif isinstance(value, list):
+                fd[key] = fields_to_enum(value, fields, _enum, default)
+        return fd
+    if isinstance(data, list):
+        return [make_dict_field_enum(d, fields, _enum, default) for d in data]
+    else:
+        return make_dict_field_enum(data, fields, _enum, default)
+
