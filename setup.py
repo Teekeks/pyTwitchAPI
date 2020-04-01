589a3ca2eca7469f546a4a5e4a74af72f14e79a8
Teekeks <info@teawork.de>
2020-04-01 19:21:30 +0200

    fixed missing files, bumped pypi version to 1.1

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 #  Copyright (c) 2020. Lena "Teekeks" During <info@teawork.de>
 
-from setuptools import setup
-setup()
+from setuptools import setup, find_packages
+setup(
+    packages=find_packages(),
+    package_data={
+        "twitchAPI": ["*.html"]
+    },
+    install_requires=['requests', 'python-dateutil', 'aiohttp']
+)
