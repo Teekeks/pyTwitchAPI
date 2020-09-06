136f65c5bec55880dce41709f48855e043c78226
Teekeks <info@teawork.de>
2020-09-06 22:08:20 +0200

    bump version on build script

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 setup(
     packages=find_packages(),
     name="twitchAPI",
-    version="1.2.3",
+    version="1.3",
     url="https://github.com/Teekeks/pyTwitchAPI",
     author="Lena \"Teekeks\" During",
     author_email="info@teawork.de",
-    description="A Python 3.7 implementation of the Twitch Helix API and its Webhook",
+    description="A Python 3.7+ implementation of the Twitch Helix API and its Webhook",
     license="MIT",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
