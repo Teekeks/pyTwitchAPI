fdcd32011e29ea0e98fdf27cf357ed65563371f4
Teekeks <info@teawork.de>
2020-10-10 04:25:09 +0200

    updated docu of get_webhook_subscriptions fixed signature of get_webhook_subscriptions

--- twitchAPI/twitch.py
+++ twitchAPI/twitch.py
@@ -1277,15 +1277,24 @@ class Twitch:
         return data
 
     def get_webhook_subscriptions(self,
-                                  first: Optional[str] = None,
+                                  first: Optional[int] = 20,
                                   after: Optional[str] = None) -> dict:
-        """Requires App authentication\n
+        """Gets the Webhook subscriptions of the authenticated user, in order of expiration.\n\n
+
+        Requires App authentication\n
         For detailed documentation, see here: https://dev.twitch.tv/docs/api/reference#get-webhook-subscriptions
 
-        :param first: optional str
-        :param after: optional str
+        :param int first: Number of values to be returned per page. Limit: 100. Default: 20.
+        :param str after: Cursor for forward pagination
+        :raises ~twitchAPI.types.UnauthorizedException: if app authentication is not set
+        :raises ~twitchAPI.types.TwitchAuthorizationException: if the used authentication token became invalid
+                        and a re authentication failed
+        :raises ~twitchAPI.types.TwitchBackendException: if the Twitch API itself runs into problems
+        :raises ValueError: if first is not in range 1 to 100
         :rtype: dict
         """
+        if first < 1 or first > 100:
+            raise ValueError('first must be in range 1 to 100')
         url = build_url(TWITCH_API_BASE_URL + 'webhooks/subscriptions',
                         {'first': first, 'after': after},
                         remove_none=True)
