6c85b5d1879fce683dee3ea04fb9cfefef1ca25f
Teekeks <info@teawork.de>
2020-10-16 15:52:03 +0200

    bump version in buildtool

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     packages=find_packages(),
     name="twitchAPI",
-    version="1.3",
+    version="2.0",
     url="https://github.com/Teekeks/pyTwitchAPI",
     author="Lena \"Teekeks\" During",
     author_email="info@teawork.de",
