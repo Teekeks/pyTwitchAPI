22e52d2763a56f35a1055cfb84f2dbfd519f8454
Teekeks <info@teawork.de>
2020-11-11 01:14:12 +0100

    added version var

--- twitchAPI/__init__.py
+++ twitchAPI/__init__.py
@@ -3,3 +3,5 @@ from twitchAPI.webhook import TwitchWebHook
 from twitchAPI.oauth import UserAuthenticator, refresh_access_token
 import twitchAPI.types
 from twitchAPI.pubsub import PubSub
+
+VERSION = (2, 1, 0)
