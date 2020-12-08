44158b59e105afaac7235c1913689cd5ade4ac7a
Teekeks <info@teawork.de>
2020-12-08 07:25:20 +0100

    final preparations for 2.2.0 release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     packages=find_packages(),
     name="twitchAPI",
-    version="2.1",
+    version="2.2.0",
     url="https://github.com/Teekeks/pyTwitchAPI",
     author="Lena \"Teekeks\" During",
     author_email="info@teawork.de",
