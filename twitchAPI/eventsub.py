0047d14e3408fae76d209ece256bfc5f2499d5f4
Teekeks <info@teawork.de>
2021-09-08 17:05:55 +0200

    added listen_channel_subscription_message

--- twitchAPI/eventsub.py
+++ twitchAPI/eventsub.py
@@ -337,3 +337,22 @@ class EventSub:
         :rtype: bool
         """
         return self._subscribe('channel.subscription.gift', '1', {'broadcaster_user_id': broadcaster_user_id}, callback)
+
+    def listen_channel_subscription_message(self, broadcaster_user_id: str, callback: Callable[[dict], None]) -> str:
+        """A notification when a user sends a resubscription chat message in a specific channel.
+
+        For more information see here: https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types#channelsubscriptionmessage
+
+        :param str broadcaster_user_id: the id of the user you want to listen to
+        :param Callable[[dict],None] callback: function for callback
+        :raises ~twitchAPI.types.EventSubSubscriptionConflict: if a conflict was found with this subscription
+            (e.g. already subscribed to this exact topic)
+        :raises ~twitchAPI.types.EventSubSubscriptionTimeout: if :code:`wait_for_subscription_confirm`
+            is true and the subscription was not fully confirmed in time
+        :raises ~twitchAPI.types.EventSubSubscriptionError: if the subscription failed (see error message for details)
+        :rtype: bool
+        """
+        return self._subscribe('channel.subscription.message',
+                               '1',
+                               {'broadcaster_user_id': broadcaster_user_id},
+                               callback)
