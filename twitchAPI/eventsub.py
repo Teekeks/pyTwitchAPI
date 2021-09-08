830547c302fd5ed33b9cfe3f23f33bd07f43182b
Teekeks <info@teawork.de>
2021-09-08 17:42:27 +0200

    added listen_channel_prediction_begin

--- twitchAPI/eventsub.py
+++ twitchAPI/eventsub.py
@@ -656,3 +656,22 @@ class EventSub:
                                '1',
                                {'broadcaster_user_id': broadcaster_user_id},
                                callback)
+
+    def listen_channel_prediction_begin(self, broadcaster_user_id: str, callback: Callable[[dict], None]) -> str:
+        """A Prediction started on a specified channel.
+
+        For more information see here: https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types#channelpredictionbegin
+
+        :param str broadcaster_user_id: the id of the user you want to listen to
+        :param Callable[[dict],None] callback: function for callback
+        :raises ~twitchAPI.types.EventSubSubscriptionConflict: if a conflict was found with this subscription
+            (e.g. already subscribed to this exact topic)
+        :raises ~twitchAPI.types.EventSubSubscriptionTimeout: if :code:`wait_for_subscription_confirm`
+            is true and the subscription was not fully confirmed in time
+        :raises ~twitchAPI.types.EventSubSubscriptionError: if the subscription failed (see error message for details)
+        :rtype: bool
+        """
+        return self._subscribe('channel.prediction.begin',
+                               '1',
+                               {'broadcaster_user_id': broadcaster_user_id},
+                               callback)
