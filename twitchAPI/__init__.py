f7b5802e68805ae84fbc4d6e0226d28f730ad16e
Teekeks <info@teawork.de>
2022-02-12 17:24:39 +0100

    removed webhook import from init

--- twitchAPI/__init__.py
+++ twitchAPI/__init__.py
@@ -1,5 +1,4 @@
 from twitchAPI.twitch import Twitch
-from twitchAPI.webhook import TwitchWebHook
 from twitchAPI.oauth import UserAuthenticator, refresh_access_token, validate_token, revoke_token
 from twitchAPI.types import *
 from twitchAPI.pubsub import PubSub
