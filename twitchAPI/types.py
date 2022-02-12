9f574ba21a02ce358db351f88561d7cf3b4bb000
Teekeks <info@teawork.de>
2022-02-12 17:09:51 +0100

    added messing scope channel:manage:chat_settings

--- twitchAPI/types.py
+++ twitchAPI/types.py
@@ -51,6 +51,7 @@ class AuthScope(Enum):
     CHANNEL_MANAGE_PREDICTIONS = 'channel:manage:predictions'
     MODERATOR_MANAGE_AUTOMOD = 'moderator:manage:automod'
     CHANNEL_MANAGE_SCHEDULE = 'channel:manage:schedule'
+    CHANNEL_MANAGE_CHAT_SETTINGS = 'channel:manage:chat_settings'
 
 
 class ModerationEventType(Enum):
