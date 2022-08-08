e7c0e42547968ded25c7f1abfd90029468c2cc09
Teekeks <info@teawork.de>
2022-08-09 01:44:25 +0200

    added a helper which allows you to automatically paginate over all pages for any api function that allows forward pagination

--- twitchAPI/__init__.py
+++ twitchAPI/__init__.py
@@ -4,5 +4,31 @@ from twitchAPI.types import *
 from twitchAPI.pubsub import PubSub
 from twitchAPI.eventsub import EventSub
 from twitchAPI.chat import *
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Callable
 
 VERSION = (3, 0, 0)
+
+
+def paginator(func: Callable[..., dict], *args, **kwargs) -> dict:
+    """Generator which allows to automatically paginate forwards for functions that allow that functionality.
+    Pass any arguments you would pass to the specified function to this function.
+
+    Example usage:
+    .. code-block:: python
+        for page in paginator(twitch.get_users, to_id=user_id):
+            print(page)
+
+    :param Callable func: The function you want to paginate over
+    :raises ValueError: if the given function does not support pagination
+    """
+    if 'after' not in func.__code__.co_varnames:
+        raise ValueError('The passed function does not support forward pagination')
+    result = func(*args, **kwargs)
+    yield result
+    while result.get('pagination', {}).get('cursor') is not None:
+        pag = result.get('pagination', {}).get('cursor')
+        result = func(*args, after=pag, **kwargs)
+        yield result
