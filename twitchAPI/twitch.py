ab64181031095b90ab2d841a9b752f59fc31f089
Teekeks <info@teawork.de>
2022-09-29 22:51:18 +0200

    added remove_channel_moderator endpoint, closes #118

--- twitchAPI/twitch.py
+++ twitchAPI/twitch.py
@@ -3521,3 +3521,27 @@ class Twitch:
         error_handler = {422: ValueError('User is a vip')}
         await self._build_result('POST', 'moderation/moderators', param, AuthType.USER, [AuthScope.CHANNEL_MANAGE_MODERATORS], None,
                                  error_handler=error_handler)
+
+    async def remove_channel_moderator(self,
+                                       broadcaster_id: str,
+                                       user_id: str):
+        """Removes a moderator from the broadcaster’s chat room.
+
+        Requires User Authentication with :const:`twitchAPI.types.AuthScope.CHANNEL_MANAGE_MODERATORS`\n
+        For detailed documentation, see here: https://dev.twitch.tv/docs/api/reference#remove-channel-moderator
+
+        :param str broadcaster_id: The ID of the broadcaster that owns the chat room.
+        :param str user_id: The ID of the user to remove as a moderator from the broadcaster’s chat room.
+        :raises ~twitchAPI.types.TwitchAPIException: if the request was malformed
+        :raises ~twitchAPI.types.UnauthorizedException: if user authentication is not set or invalid
+        :raises ~twitchAPI.types.MissingScopeException: if the user authentication is missing the required scope
+        :raises ~twitchAPI.types.TwitchAuthorizationException: if the used authentication token became invalid
+                        and a re authentication failed
+        :raises ~twitchAPI.types.TwitchBackendException: if the Twitch API itself runs into problems
+        :raises ~twitchAPI.types.TwitchAPIException: if a Query Parameter is missing or invalid
+        """
+        param = {
+            'broadcaster_id': broadcaster_id,
+            'user_id': user_id
+        }
+        await self._build_result('DELETE', 'moderation/moderators', param, AuthType.USER, [AuthScope.CHANNEL_MANAGE_MODERATORS], None)
