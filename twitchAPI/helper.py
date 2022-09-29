3c409cf4d9284bd4e21b5eaa938baa6b210eb25e
Teekeks <info@teawork.de>
2022-09-29 16:56:26 +0200

    added first to `__all__`

--- twitchAPI/helper.py
+++ twitchAPI/helper.py
@@ -16,7 +16,7 @@ from typing import Union, List, Type, Optional, Callable, Generator
 
 __all__ = ['TWITCH_API_BASE_URL', 'TWITCH_AUTH_BASE_URL', 'TWITCH_PUB_SUB_URL', 'TWITCH_CHAT_URL',
            'extract_uuid_str_from_url', 'build_url', 'get_uuid', 'get_json', 'make_fields_datetime', 'build_scope', 'fields_to_enum', 'make_enum',
-           'enum_value_or_none', 'datetime_to_str', 'remove_none_values', 'ResultType']
+           'enum_value_or_none', 'datetime_to_str', 'remove_none_values', 'ResultType', 'first']
 
 TWITCH_API_BASE_URL = "https://api.twitch.tv/helix/"
 TWITCH_AUTH_BASE_URL = "https://id.twitch.tv/"
