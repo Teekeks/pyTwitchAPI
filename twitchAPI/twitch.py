f7cfb1f131d4db5e7d36b7b71c5fc8eb1c6691a8
Teekeks <info@teawork.de>
2022-09-29 22:48:14 +0200

    added add_channel_moderator endpoint, closes #119

--- twitchAPI/twitch.py
+++ twitchAPI/twitch.py
@@ -3494,3 +3494,30 @@ class Twitch:
         async for y in self._build_generator('GET', 'channels/vips', param, AuthType.USER, [AuthScope.CHANNEL_READ_VIPS], ChannelVIP,
                                              split_lists=True):
             yield y
+
+    async def add_channel_moderator(self,
+                                    broadcaster_id: str,
+                                    user_id: str):
+        """Adds a moderator to the broadcaster’s chat room.
+
+        Requires User Authentication with :const:`twitchAPI.types.AuthScope.CHANNEL_MANAGE_MODERATORS`\n
+        For detailed documentation, see here: https://dev.twitch.tv/docs/api/reference#add-channel-moderator
+
+        :param str broadcaster_id: The ID of the broadcaster that owns the chat room.
+        :param str user_id: The ID of the user to add as a moderator in the broadcaster’s chat room.
+        :raises ~twitchAPI.types.TwitchAPIException: if the request was malformed
+        :raises ~twitchAPI.types.UnauthorizedException: if user authentication is not set or invalid
+        :raises ~twitchAPI.types.MissingScopeException: if the user authentication is missing the required scope
+        :raises ~twitchAPI.types.TwitchAuthorizationException: if the used authentication token became invalid
+                        and a re authentication failed
+        :raises ~twitchAPI.types.TwitchBackendException: if the Twitch API itself runs into problems
+        :raises ~twitchAPI.types.TwitchAPIException: if a Query Parameter is missing or invalid
+        :raises ValueError: If user is a vip
+        """
+        param = {
+            'broadcaster_id': broadcaster_id,
+            'user_id': user_id
+        }
+        error_handler = {422: ValueError('User is a vip')}
+        await self._build_result('POST', 'moderation/moderators', param, AuthType.USER, [AuthScope.CHANNEL_MANAGE_MODERATORS], None,
+                                 error_handler=error_handler)
