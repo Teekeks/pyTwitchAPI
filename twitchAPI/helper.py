39654d18ee31c98b9fcccbc1f17e5b1980e9dea8
Teekeks <info@teawork.de>
2023-02-11 11:56:13 +0100

    expanded helper type hinting and docs

--- twitchAPI/helper.py
+++ twitchAPI/helper.py
@@ -3,6 +3,7 @@
 Helper functions
 ----------------"""
 import asyncio
+import datetime
 import logging
 import time
 import urllib.parse
@@ -14,19 +15,19 @@ from .types import AuthScope
 
 from typing import Union, List, Type, Optional
 
-__all__ = ['TWITCH_API_BASE_URL', 'TWITCH_AUTH_BASE_URL', 'TWITCH_PUB_SUB_URL', 'TWITCH_CHAT_URL',
+__all__ = ['first', 'limit', 'TWITCH_API_BASE_URL', 'TWITCH_AUTH_BASE_URL', 'TWITCH_PUB_SUB_URL', 'TWITCH_CHAT_URL',
            'build_url', 'get_uuid', 'build_scope', 'fields_to_enum', 'make_enum',
-           'enum_value_or_none', 'datetime_to_str', 'remove_none_values', 'ResultType', 'first', 'limit', 'RateLimitBucket', 'RATE_LIMIT_SIZES']
+           'enum_value_or_none', 'datetime_to_str', 'remove_none_values', 'ResultType', 'RateLimitBucket', 'RATE_LIMIT_SIZES']
 
 T = TypeVar('T')
 
-TWITCH_API_BASE_URL = "https://api.twitch.tv/helix/"
+TWITCH_API_BASE_URL: str = "https://api.twitch.tv/helix/"
 """The base url to the Twitch API endpoints"""
-TWITCH_AUTH_BASE_URL = "https://id.twitch.tv/"
+TWITCH_AUTH_BASE_URL: str = "https://id.twitch.tv/"
 """The base url to the twitch authentication endpoints"""
-TWITCH_PUB_SUB_URL = "wss://pubsub-edge.twitch.tv"
+TWITCH_PUB_SUB_URL: str = "wss://pubsub-edge.twitch.tv"
 """The url to the Twitch PubSub websocket"""
-TWITCH_CHAT_URL = "wss://irc-ws.chat.twitch.tv:443"
+TWITCH_CHAT_URL: str = "wss://irc-ws.chat.twitch.tv:443"
 """The url to the Twitch Chat websocket"""
 
 
@@ -41,12 +42,10 @@ def build_url(url: str, params: dict, remove_none: bool = False, split_lists: bo
 
     :param url: base URL
     :param params: dictionary of URL parameter
-    :param bool remove_none: if set all params that have a None value get removed |default| :code:`False`
-    :param bool split_lists: if set all params that are a list will be split over multiple url
-            parameter with the same name |default| :code:`False`
-    :param bool enum_value: if true, automatically get value string from Enum values |default| :code:`True`
+    :param remove_none: if set all params that have a None value get removed |default| :code:`False`
+    :param split_lists: if set all params that are a list will be split over multiple url parameter with the same name |default| :code:`False`
+    :param enum_value: if true, automatically get value string from Enum values |default| :code:`True`
     :return: URL
-    :rtype: str
     """
 
     def get_val(val):
@@ -84,8 +83,7 @@ def get_uuid() -> uuid.UUID:
 def build_scope(scopes: List[AuthScope]) -> str:
     """Builds a valid scope string from list
 
-    :param list[~twitchAPI.types.AuthScope] scopes: list of :class:`~twitchAPI.types.AuthScope`
-    :rtype: str
+    :param scopes: list of :class:`~twitchAPI.types.AuthScope`
     :returns: the valid auth scope string
     """
     return ' '.join([s.value for s in scopes])
@@ -95,13 +93,12 @@ def fields_to_enum(data: Union[dict, list],
                    fields: List[str],
                    _enum: Type[Enum],
                    default: Optional[Enum]) -> Union[dict, list]:
-    """Itterates a dict or list and tries to replace every dict entry with key in fields with the correct Enum value
+    """Iterates a dict or list and tries to replace every dict entry with key in fields with the correct Enum value
 
-    :param Union[dict, list] data: dict or list
-    :param list[str] fields: list of keys to be replaced
+    :param data: dict or list
+    :param fields: list of keys to be replaced
     :param _enum: Type of Enum to be replaced
     :param default: The default value if _enum does not contain the field value
-    :rtype: dict or list
     """
     _enum_vals = [e.value for e in _enum.__members__.values()]
 
@@ -135,7 +132,12 @@ def fields_to_enum(data: Union[dict, list],
         return make_dict_field_enum(data, fields, _enum, default)
 
 
-def make_enum(data: str, _enum: Type[Enum], default: Enum) -> Enum:
+def make_enum(data: Union[str, int], _enum: Type[Enum], default: Enum) -> Enum:
+    """Takes in a value and maps it to the given Enum. If the value is not valid it will take the default.
+
+    :param data: the value to map from
+    :param _enum: the Enum type to map to
+    :param default: the default value"""
     _enum_vals = [e.value for e in _enum.__members__.values()]
     if data in _enum_vals:
         return _enum(data)
@@ -143,17 +145,25 @@ def make_enum(data: str, _enum: Type[Enum], default: Enum) -> Enum:
         return default
 
 
-def enum_value_or_none(enum):
+def enum_value_or_none(enum: Optional[Enum]) -> Union[None, str, int]:
+    """Returns the value of the given Enum member or None
+
+    :param enum: the Enum member"""
     return enum.value if enum is not None else None
 
 
-def datetime_to_str(dt):
+def datetime_to_str(dt: Optional[datetime.datetime]) -> Optional[str]:
+    """ISO-8601 formats the given datetime, returns None if datetime is None
+
+    :param dt: the datetime to format"""
     return dt.astimezone().isoformat() if dt is not None else None
 
 
 def remove_none_values(d: dict) -> dict:
     """Removes items where the value is None from the dict.
-    This returns a new dict and does not manipulate the one given."""
+    This returns a new dict and does not manipulate the one given.
+
+    :param d: the dict from which the None values should be removed"""
     return {k: v for k, v in d.items() if v is not None}
 
 
@@ -204,7 +214,14 @@ class RateLimitBucket:
                  bucket_length: int,
                  bucket_size: int,
                  scope: str,
-                 logger=None):
+                 logger: Optional[logging.Logger] = None):
+        """
+
+        :param bucket_length: time in seconds the bucket is valid for
+        :param bucket_size: the number of entries that can be put into the bucket
+        :param scope: the scope of this bucket (used for logging)
+        :param logger: the logger to be used. If None the default logger is used
+        """
         self.scope = scope
         self.bucket_length = float(bucket_length)
         self.bucket_size = bucket_size
@@ -237,6 +254,9 @@ class RateLimitBucket:
             logging.warning(msg)
 
     async def put(self, num: int = 1):
+        """Puts :code:`num` usees into the current bucket and waits if rate limit is hit
+
+        :param num: the number of uses put into the current bucket"""
         async with self.lock:
             delta = self.get_delta(num)
             if delta is not None:
