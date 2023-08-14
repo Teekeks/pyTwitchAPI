c06f987c8a13cd15f2face87025a2dde201f9e74
Teekeks <info@teawork.de>
2023-08-14 23:43:08 +0200

    refactor: removed default import from `__init__`

--- twitchAPI/__init__.py
+++ twitchAPI/__init__.py
@@ -1,9 +1,3 @@
-from .twitch import Twitch
-from .eventsub.webhook import EventSubWebhook
-from .pubsub import PubSub
-from .oauth import UserAuthenticator
-from .chat import Chat
-
 VERSION = (3, 11, 0)
 
 __version__ = '3.11.0'
